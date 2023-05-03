9b8c6ccb0ff120fb8f6162005bfa4acf4ac8fba7
Nabeel-nazeer <nabeelnazeer2565@gmail.com>
2023-05-03 11:46:27 +0530

    commit

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ import pygame
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("rocket saga")
+
+WHITE = (255, 255, 255)
 
 def main():
 
-    run = true
+    run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-     
-             run = false
+                run = False
+        WIN.fill(WHITE)
+        pygame.display.update(
+            
+        )    
     pygame.quit()
 if __name__ == "__main__":
     main()                 
